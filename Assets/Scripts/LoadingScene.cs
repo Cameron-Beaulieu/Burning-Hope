c1c769ee59f56e90eb8962f7fb3bb1f4da7c7685
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-03-31 22:00:31 -0400

    add main menu and loading screen

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    private float textUpdateRate = 0.5f;
+    private float textTimer;
+    private string[] text = {
+        "Loading",
+        "Loading.",
+        "Loading..",
+        "Loading..."
+    };
+
+    private int currentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = text[0];
+        currentText = 0;
+        textTimer = 0f;
+        StartCoroutine(LoadAsync());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textTimer += Time.deltaTime;
+        if (textTimer > textUpdateRate)
+        {
+            textTimer = 0f;
+            currentText = currentText + 1;
+
+            if (currentText == text.Length)
+            {
+                currentText = 0;
+            }
+
+            gameObject.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = text[currentText];
+        }
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(GlobalState.Scene);
+        yield return new WaitForEndOfFrame();
+    }
+}
