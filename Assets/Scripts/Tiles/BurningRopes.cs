884316c0632eb6678a299cd46b65a03821dcd9b9
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-04-04 21:12:57 -0400

    add ability for ropes to light braziers

--- Assets/Scripts/Tiles/BurningRopes.cs
+++ Assets/Scripts/Tiles/BurningRopes.cs
@@ -8,10 +8,13 @@ public class BurningRopes : MonoBehaviour
     public TileBase unlitTile;
     public TileBase litTile;
     private Tilemap tilemap;
+    private List<BrazierGroup> brazierControllers = new List<BrazierGroup>();
+    private List<Tilemap> brazierMaps = new List<Tilemap>();
     public GameObject lightPrefab;
     private Dictionary<Vector2Int, float> ropeTimers = new Dictionary<Vector2Int, float>();
     private Dictionary<Vector2Int, bool> ropeBurning = new Dictionary<Vector2Int, bool>();
     private Dictionary<Vector2Int, GameObject> ropeLights = new Dictionary<Vector2Int, GameObject>();
+    private List<Vector3> braziers = new List<Vector3>();
     public float burnTime = 0.5f;
 
     // Start is called before the first frame update
@@ -27,6 +30,25 @@ public class BurningRopes : MonoBehaviour
                 ropeTimers.Add((Vector2Int)loc, burnTime);
                 ropeBurning.Add((Vector2Int)loc, false);
                 tilemap.SetTile(loc, unlitTile);
+            }
+        }
+
+        // Find all braziers
+        GameObject[] objects = GameObject.FindObjectsOfType<GameObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].layer == LayerMask.NameToLayer("Braziers"))
+            {
+                brazierControllers.Add(objects[i].GetComponent<BrazierGroup>());
+                brazierMaps.Add(objects[i].GetComponent<Tilemap>());
+                foreach (var pos in brazierMaps[brazierMaps.Count - 1].cellBounds.allPositionsWithin)
+                {
+                    Vector3Int loc = new Vector3Int(pos.x, pos.y, pos.z);
+                    if (brazierMaps[brazierMaps.Count - 1].HasTile(loc))
+                    {
+                        braziers.Add(brazierMaps[brazierMaps.Count - 1].CellToWorld(loc));
+                    }
+                }
             }
         }
     }
@@ -50,14 +72,42 @@ public class BurningRopes : MonoBehaviour
                 // burn surrounding ropes
                 Vector2Int up = rope.Key + Vector2Int.up;
                 LightRopeCell(up, true);
+                Vector3 brazierUp = tilemap.CellToWorld((Vector3Int)up);
+                if (braziers.Contains(brazierUp))
+                {
+                    LightBrazier(brazierUp);
+                }
                 Vector2Int down = rope.Key + Vector2Int.down;
                 LightRopeCell(down, true);
+                Vector3 brazierDown = tilemap.CellToWorld((Vector3Int)down);
+                if (braziers.Contains(brazierDown))
+                {
+                    LightBrazier(brazierDown);
+                }
                 Vector2Int left = rope.Key + Vector2Int.left;
                 LightRopeCell(left, true);
+                Vector3 brazierLeft = tilemap.CellToWorld((Vector3Int)left);
+                if (braziers.Contains(brazierLeft))
+                {
+                    LightBrazier(brazierLeft);
+                }
                 Vector2Int right = rope.Key + Vector2Int.right;
                 LightRopeCell(right, true);
+                Vector3 brazierRight = tilemap.CellToWorld((Vector3Int)right);
+                if (braziers.Contains(brazierRight))
+                {
+                    LightBrazier(brazierRight);
+                }
                 tilemap.SetTile((Vector3Int)rope.Key, null);
             }
+        }
+    }
+
+    public void LightBrazier(Vector3 pos)
+    {
+        for (int i = 0; i < brazierControllers.Count; i++)
+        {
+            brazierControllers[i].LightBrazier(pos, true);
         }
     }
 
