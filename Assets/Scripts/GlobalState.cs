90fe6e35483029ce9d6f8ab9ca51139e62c34de7
c-kip <callumkipin@gmail.com>
2021-04-07 00:28:55 -0400

    Added timers to each level and displays of level time between each level

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public static class GlobalState
 {
+    public static float Time { get; set; }
+    public static float CaveTime { get; set; }
+    public static float MineTime { get; set; }
+    public static float CultTime { get; set; }
     public static int Scene { get; set; }
     public static void LoadScene(int scene)
     {
@@ -13,6 +17,6 @@ public static class GlobalState
     }
     public static void LoadLevel(int level)
     {
-        LoadScene(1 + level);
+        LoadScene(2 * level); //Level 1 is scene 2, level 2 is scene 4, etc.
     }
 }
